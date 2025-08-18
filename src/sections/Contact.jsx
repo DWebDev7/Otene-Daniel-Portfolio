18efa3119ea28a4e3b95abba1c93d8ccbed5f83c
OTG12 <oteneezekiel04@gmail.com>
2025-08-18 20:28:35 +0100

    Added Tailwind Link

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -122,7 +122,7 @@ const Contact = () => {
       </form>
 
       {isSuccess && (
-        <div className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-green-500 text-white px-4 py-2 rounded-md shadow-lg">
+        <div className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-purple-600 text-white px-4 py-2 rounded-md shadow-lg">
           Message sent successfully!
         </div>
       )}
